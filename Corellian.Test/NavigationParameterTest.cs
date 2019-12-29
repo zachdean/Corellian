205c6310b7a3a22bea0ffbaed9048673dc278d51
Zach Dean <zach.dean@daveramsey.com>
2019-12-29 10:23:53 -0600

    add extension methods

---
+++ Corellian.Test/NavigationParameterTest.cs
@@ -0,0 +1,116 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Corellian.Test
+{
+    public class NavigationParameterTest
+    {
+        private readonly INavigationParameter navigationParameter;
+        private const string Enum = "Enum";
+        private const string String = "String";
+        private const string Class = "TestClass";
+        private const string Struct = "TestStruct";
+
+
+
+        public NavigationParameterTest()
+        {
+            navigationParameter = new NavigationParameter 
+            {
+                {Enum, TestEnum.Three },
+                {String, String },
+                {Class, TestClass.GetClass()},
+                {Struct, TestStruct.GetStruct() }
+            };
+        }
+
+        [Fact]
+        public void GetParameter_WithClass_ExpectClass()
+        {
+            var result = navigationParameter.GetParameter<TestClass>(Class);
+
+            result.Should().BeEquivalentTo(TestClass.GetClass());
+        }
+
+        [Fact]
+        public void GetParameter_WithClassWrongKey_ExpectNull()
+        {
+            var result = navigationParameter.GetParameter<TestClass>("Invalid");
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetParameter_WithClassWrongType_ExpectNull()
+        {
+            var result = navigationParameter.GetParameter<TestClass>(Struct);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetParameter_WithStruct_ExpectStruct()
+        {
+            var result = navigationParameter.GetParameter<TestStruct>(Struct);
+
+            result.Should().BeEquivalentTo(TestStruct.GetStruct());
+        }
+
+        [Fact]
+        public void GetParameter_WithEnum_ExpectThree()
+        {
+            var result = navigationParameter.GetParameter<TestEnum>(Enum);
+
+            result.Should().Be(TestEnum.Three);
+        }
+
+
+        [Fact]
+        public void GetRequiredParameter_WithEnum_ExpectThree()
+        {
+            var result = navigationParameter.GetRequiredParameter<TestEnum>(Enum);
+
+            result.Should().Be(TestEnum.Three);
+        }
+
+        [Fact]
+        public void GetRequiredParameter_WithWrongKey_ExpectKeyNotFoundException()
+        {
+            var result = Record.Exception(() => navigationParameter.GetRequiredParameter<TestEnum>("Invalid"));
+
+            result.Should().BeOfType<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public void GetRequiredParameter_WithWrongType_ExpectKeyNotFoundException()
+        {
+            var result = Record.Exception(() => navigationParameter.GetRequiredParameter<TestEnum>(String));
+
+            result.Should().BeOfType<InvalidCastException>();
+        }
+
+        private enum TestEnum
+        {
+            One,
+            Two,
+            Three
+        }
+
+        private class TestClass
+        {
+            public static TestClass GetClass() => new TestClass {MyProperty = 6, TestProperty = -5};
+            public int MyProperty { get; set; }
+            public double TestProperty { get; set; }
+        }
+
+        private struct TestStruct
+        {
+            public static TestStruct GetStruct() => new TestStruct { MyProperty = 6, TestProperty = -5 };
+            public int MyProperty { get; set; }
+            public double TestProperty { get; set; }
+        }
+    }
+}
--- Corellian/Extensions/INavigationParameterExtensions.cs
+++ Corellian/Extensions/INavigationParameterExtensions.cs
@@ -1,7 +1,51 @@
-﻿namespace Corellian.Core.Extensions
+﻿using System;
+using System.Collections.Generic;
+
+namespace Corellian
 {
     public static class INavigationParameterExtensions
     {
-        
+        /// <summary>
+        /// Gets the parameter, returns defualt if not found or type is invalid
+        /// </summary>
+        /// <typeparam name="T">Parameter Type</typeparam>
+        /// <param name="parameter">Navigation Paramter</param>
+        /// <param name="key">Paramter Key</param>
+        /// <returns></returns>
+        public static T GetParameter<T>(this INavigationParameter parameter, string key)
+        {
+            if (!parameter.TryGetValue(key, out var result))
+            {
+                return default;
+            }
+
+            if (result is T castResult)
+            {
+                return castResult;
+            }
+
+            try
+            {
+                return (T)Convert.ChangeType(result, typeof(T));
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// Gets the required parameter
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <param name="parameter">Navigation Parameter</param>
+        /// <param name="key">parameter key</param>
+        /// <returns>pass parameter</returns>
+        /// <exception cref="KeyNotFoundException" />
+        /// <exception cref="InvalidCastException" />
+        public static T GetRequiredParameter<T>(this INavigationParameter parameter, string key)
+        {
+            return (T)parameter[key];
+        }
     }
 }
